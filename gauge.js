5a2992a1b04be7cccf714c31ad54e530d3586770
Bev Edwards-Smith <bedwardssmith@yahoo.ca>
2021-03-14 14:59:28 -0400

    color

--- gauge.js
+++ gauge.js
@@ -118,7 +118,7 @@ function buildCharts(sample) {
       marker: {
         size: values,
         color: values,
-        colorscale: "RdBu"
+        colorscale: "PRGn"
       }
     }];
     // Create the layout for the bubble chart.
