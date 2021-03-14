2d9852e994018fe04b1d1814918347214060a912
bedwardssmith <69326607+bedwardssmith@users.noreply.github.com>
2021-03-14 13:36:26 -0400

    change to color

--- bubble.js
+++ bubble.js
@@ -109,7 +109,7 @@ function buildCharts(sample) {
       marker: {
         size: values,
         color: values,
-        colorscale: "balance"
+        colorscale: "Temps"
       }
     }];
     // 2. Create the layout for the bubble chart.
