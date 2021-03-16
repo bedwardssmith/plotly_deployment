88cb4e012dc2b62d490f85d7f371b1a9c4013397
bedwardssmith <69326607+bedwardssmith@users.noreply.github.com>
2021-03-16 19:54:58 -0400

    Update charts.js

--- charts.js
+++ charts.js
@@ -128,7 +128,8 @@ function buildCharts(sample) {
       hovermode: "closest",
       margin: {
         l: 400,
-        r: 400}
+        r: 400,
+        pad: 400}
     };
     // D2: 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout)
