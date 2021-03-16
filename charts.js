764db019793ba1518fb01c2249c4c240279260d1
bedwardssmith <69326607+bedwardssmith@users.noreply.github.com>
2021-03-16 06:48:33 -0400

    color

--- charts.js
+++ charts.js
@@ -118,7 +118,7 @@ function buildCharts(sample) {
       marker: {
         size: values,
         color: values,
-        colorscale: "teal"
+        colorscale: "balance"
       }
     }];
     // Create the layout for the bubble chart.
