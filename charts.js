b92a0f9df76ca04dbc282cbd4a10b77d935b014f
bedwardssmith <69326607+bedwardssmith@users.noreply.github.com>
2021-03-16 06:45:51 -0400

    Update charts.js

--- charts.js
+++ charts.js
@@ -118,7 +118,7 @@ function buildCharts(sample) {
       marker: {
         size: values,
         color: values,
-        colorscale: "deep"
+        colorscale: "teal"
       }
     }];
     // Create the layout for the bubble chart.
