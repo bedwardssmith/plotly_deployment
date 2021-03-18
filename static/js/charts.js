8646b7808f5601dd130a70462f221aba7519da40
bedwardssmith <69326607+bedwardssmith@users.noreply.github.com>
2021-03-18 15:30:35 -0400

    removed padding from title

--- static/js/charts.js
+++ static/js/charts.js
@@ -124,7 +124,7 @@ function buildCharts(sample) {
     }];
     // Create the layout for the bubble chart.
     var bubbleLayout = {
-      title: {pad: 800, text: "<b>Bacteria Cultures per Sample</b>"}, 
+      title: {text: "<b>Bacteria Cultures per Sample</b>"}, 
       font: {color: "MidnightBlue"},      
       xaxis: {title: "<b>OTU ID</b>"},
       hovermode: "closest", 
